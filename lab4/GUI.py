337b6f5f97bdb381a349b24e86ebb25eadc1b5aa
Volodymyr Tkach <vovantkac5@gmail.com>
2023-04-26 15:29:09 +0300

    Lab4

--- lab4/GUI.py
+++ lab4/GUI.py
@@ -1,9 +1,11 @@
-import numpy
 import tkinter as tk
 import tkinter.font as tkFont
 from tkinter.messagebox import showerror
-import matplotlib.pyplot as plt
+
 import matplotlib
+import matplotlib.pyplot as plt
+import numpy
+
 matplotlib.use("TkAgg")
 import algorithms
 
@@ -28,15 +30,54 @@ class SolveTask(tk.Toplevel):
         self.e = tk.StringVar()
         tk.Entry(self, textvariable=self.e).grid(row=3, column=1, sticky='nse', padx=10, pady=5)
 
-        tk.Button(self, text="Обчислити", bg='white', borderwidth=2, command=self.calculate).grid(row=4, column=0, columnspan=2, padx=10, pady=5, sticky='nswe')
-
-
+        tk.Button(self, text="Обчислити", bg='white', borderwidth=2, command=self.calculate).grid(row=4, column=0,
+                                                                                                  columnspan=2, padx=10,
+                                                                                                  pady=5, sticky='nswe')
+        tk.Button(self, text="Графік рівняння", bg='white', borderwidth=2, command=self.draw_graph).grid(row=5,
+                                                                                                         column=0,
+                                                                                                         columnspan=2,
+                                                                                                         padx=10,
+                                                                                                         pady=5,
+                                                                                                         sticky='nswe')
 
     def calculate(self):
-        pass
+        try:
+            results = []
+            a = float(self.a.get())
+            b = float(self.b.get())
+            x = numpy.arange(a, b, 0.1).tolist()
 
+            for i in range(1, len(x)):
+                temp = algorithms.chord_method(algorithms.f, x[i - 1], x[i], float(self.e.get()))
+                if temp is not None:
+                    results.append(temp)
 
+        except ValueError:
+            showerror(title="Помилка", message="Границі та точність повинні бути числами!")
 
+        tk.Label(self, text="Результат:").grid(row=6, column=0, sticky='nsw', padx=10, pady=15)
+        if not results:
+            tk.Label(self, text="Коренів не знайдено").grid(row=6, column=1, sticky='nsew', padx=10, pady=15)
+        else:
+            tk.Label(self, text=f"{results}").grid(row=6, column=1, sticky='nsew', padx=10, pady=15)
+
+    def draw_graph(self):
+        x = numpy.arange(0.01, 13, 0.01).tolist()
+        y = []
+        y_0 = []
+        for i in x:
+            y.append(algorithms.f(i))
+            y_0.append(0)
+
+        fig, ax = plt.subplots(1, 1)
+
+        ax.plot(x, y, label='f = 2*lg(x) - x/2 + 1')
+        ax.plot(x, y_0, label='f = 0')
+        ax.set_xlabel("X")
+        ax.set_ylabel("Y")
+        ax.legend(loc="lower right")
+
+        plt.show()
 
 
 class Main(tk.Tk):
@@ -51,10 +92,12 @@ class Main(tk.Tk):
 
         tk.Label(self, text="Виконав: Ткач Володимир\n"
                             "Група: ІО-15\n\n"
-                            "Варіант №17 (Метод Хорд)", justify=tk.LEFT).grid(row=0, column=0, padx=15, pady=5, sticky='we')
+                            "Варіант №17 (Метод Хорд)", justify=tk.LEFT).grid(row=0, column=0, padx=15, pady=5,
+                                                                              sticky='we')
 
-        tk.Button(self, text="Розв'язати рівняння", bg='white', borderwidth=2, command=SolveTask).grid(row=1, column=0, padx=15, pady=5, sticky='nswe')
-
+        tk.Button(self, text="Розв'язати рівняння", bg='white', borderwidth=2, command=SolveTask).grid(row=1, column=0,
+                                                                                                       padx=15, pady=5,
+                                                                                                       sticky='nswe')
 
 
 if __name__ == "__main__":
--- lab4/algorithms.py
+++ lab4/algorithms.py
@@ -1,5 +1,32 @@
 from math import log10
+from tkinter.messagebox import showerror
 
 
+# my function
 def f(x):
     return 2 * log10(x) - (x / 2) + 1
+
+
+def chord_method(func, a, b, tol=1e-6, max_iter=1000):
+    try:
+        fa, fb = func(a), func(b)
+
+        if fa * fb >= 0:
+            raise ValueError("Метод хорд не гарантує збіжності на даному інтервалі")
+
+        for i in range(max_iter):
+            c = (a * func(b) - b * func(a)) / (func(b) - func(a))
+            fc = func(c)
+            if abs(fc) < tol:
+                return c
+            if fa * fc < 0:
+                b, fb = c, fc
+            else:
+                a, fa = c, fc
+
+    except ValueError:
+        return None
+
+    showerror(title="Помилка", message="Досягнута максимальна кількість ітерацій!")
+    return None
+
